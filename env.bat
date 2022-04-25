817b2af3a8b111a4b09648fee54e65509171852a
Anastasiia <103857493+nastyacode2022@users.noreply.github.com>
2022-04-25 21:00:10 +0300

    Added new tests

---
+++ env.bat
@@ -0,0 +1,8 @@
+set MACHINE=machine1
+set WP_HOST=site
+
+set WC_KEY=ck_66def9128cb372bf9fd9ecf362eb142a7ed41026
+set WC_SECRET=cs_a488a05b34bc6fbfbe53b5b3397e8b823d4af330
+
+set DB_USER=root
+set DB_PASSWORD=root
